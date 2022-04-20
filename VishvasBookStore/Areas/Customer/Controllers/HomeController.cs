210f47c6d524fa5bbb49db9b650e450ae2e3ccf7
VishvaShah01 <W0784026@myscc.ca>
2022-04-19 22:00:34 -0400

    Dipsplaying Product data on main page with image and modify the README.

--- VishvasBookStore/Areas/Customer/Controllers/HomeController.cs
+++ VishvasBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,27 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using VishvasBooks.DataAccess.Repository.IRepository;
+using VishvasBooks.Models;
+using VishvasBookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using VishvasBookStore.Models;
 
-namespace VishvasBookStore.Area.Customer.Controllers
+namespace VishvasBooks.Ares.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unifOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unifOfWork)
         {
             _logger = logger;
+            _unifOfWork = unifOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unifOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
@@ -32,7 +37,7 @@ namespace VishvasBookStore.Area.Customer.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new VishvasBookStore.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
